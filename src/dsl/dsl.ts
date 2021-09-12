b32e2a23e341a37c61bbadc22dbf6cdb9b32207a
HTTdesu <592421947@qq.com>
2021-09-12 23:31:43 +0800

    Add jmp support for script

--- src/dsl/dsl.ts
+++ src/dsl/dsl.ts
@@ -1,6 +1,45 @@
 export class DSL {
+  pc : number = 0;
+  codes : Array<string> = [];
+  variables : {[key : string] : any} = {};
+
+  load(script : string) : void {
+    this.codes = script.split("\n");
+  }
+
+  loadFromUrl(url : string, async : boolean) : void {
+  }
+
+  setVar(name : string, value : any) : void {
+    this.variables[name] = value;
+  }
+
+  getVar(name : string) : any {
+    return this.variables[name];
+  }
+
+  jmp(line : number) : void {
+    this.pc = line - 1; // skip pc++
+  }
+
+  singleStep() : void {
+    try {
+      eval(this.codes[this.pc]);
+      this.pc++;
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  execute() : void {
+    this.pc = 0;
+    while(this.pc < this.codes.length) {
+      this.singleStep();
+    }
+  }
+
   run(script: string) {
-    // TODO: ...
-    console.log("Execute any scripts ... ");
+    this.load(script);
+    this.execute();
   }
 }
--- test/dsl-test.ts
+++ test/dsl-test.ts
@@ -2,5 +2,12 @@ import { DSL } from "../src/dsl/dsl";
 
 (async () => {
   const dsl = new DSL();
-  dsl.run("...");
+  const testScript = 
+  'this.setVar("i", 0)\n'                      +
+  'console.log(this.getVar("i"));\n'           +
+  'this.setVar("i", this.getVar("i") + 1);\n'  +
+  'if(this.getVar("i") < 10) this.jmp(1);'     ;
+  
+  console.log("start!");
+  dsl.run(testScript);
 })();
